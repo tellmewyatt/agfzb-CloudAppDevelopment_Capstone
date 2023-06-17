ae13ba77b1826e74c441e5ae972ef188bd935037
tellmewyatt <wyattcannon78@gmail.com>
2023-06-17 16:12:00 -0700

    Added carr models and types

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -9,8 +9,11 @@ from django.utils.timezone import now
 # - Description
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
-
-
+class CarMake(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=200)
+    def __str__(self):
+        return self.name
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
 # - Many-To-One relationship to Car Make model (One Car Make has many Car Models, using ForeignKey field)
 # - Name
@@ -19,6 +22,23 @@ from django.utils.timezone import now
 # - Year (DateField)
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
+class CarModel(models.Model):
+    name = models.CharField(max_length=50, default="Subaru Impreza")
+    make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    dealer_id = models.IntegerField(default=0)
+    TYPE_CHOICES = [
+        ("SE", "Sedan"),
+        ("SU", "SUV"),
+        ("WA", "Station Wagon"),
+        ("VN", "Van")
+    ]
+    car_type = models.CharField(
+        max_length=2,
+        choices=TYPE_CHOICES,
+        default="SW")
+    year = models.DateField()
+    def __str__(self):
+        return self.get_car_type_display()
 
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
